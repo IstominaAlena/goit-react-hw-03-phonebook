8cccf997f1b0cbe6edbd6e89c34c7bd6add8a288
Istomina Alena <mynikisalena@gmail.com>
2021-12-13 15:10:11 +0200

    Code refactoring

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ import './styles/App.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -82,15 +77,16 @@ class App extends Component {
       id: nanoid(),
     };
 
-    this.setState(prevState => {
-      const findInArray = prevState.contacts.find(({ name }) => {
-        const lowerCaseStateName = name.toLowerCase();
-        return lowerCaseStateName === lowerCaseName;
-      });
+    const findInArray = this.state.contacts.find(({ name }) => {
+      const lowerCaseStateName = name.toLowerCase();
+      return lowerCaseStateName === lowerCaseName;
+    });
 
-      if (findInArray) {
-        return alert(`${name} is already in your contacts!`);
-      }
+    if (findInArray) {
+      return alert(`${name} is already in your contacts!`);
+    }
+
+    this.setState(prevState => {
       return {
         contacts: [...prevState.contacts, contact],
       };
